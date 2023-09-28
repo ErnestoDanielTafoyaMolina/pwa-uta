402d823155464a5913ce0c3cf8f205de466c1132
ErnestoDanielTafoyaMolina <tafoya.ernesto.1cm@gmail.com>
2023-09-28 17:28:01 -0600

    first commit

---
+++ 1DA/Modules/structs.go
@@ -0,0 +1,32 @@
+// package modules
+
+// // interfaces
+// type funcsGlobal interface {
+
+// }
+
+// // metodos y declaracion de funciones
+// type Human struct {
+// 	altura int
+// 	peso   int
+// }
+
+// type serVivo struct {
+// 	cantidadPatas int
+// }
+
+// func (h * human) test1() (err error) {
+
+// }
+// func (h * serVivo) test1() (err error) {
+
+// }
+// func test2() (asd int, dinamic ...int) {
+// 	return
+// }
+
+// test2(1,2,3,4,5,6,7,8)
+
+// func main(){
+
+// }
